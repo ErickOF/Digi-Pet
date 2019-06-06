805176118f2679f92e0b7bc687b9be955823e275
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-06-06 00:12:51 -0600

    Login: Routes

--- Presentation/DigiPet/src/app/app-routing.module.ts
+++ Presentation/DigiPet/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component'
+
+
+const routes: Routes = [
+  {
+  	path: '',
+  	redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+  	path: 'login',
+  	component: LoginComponent,
+  },
+  {
+  	path: '**',
+  	component: LoginComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
