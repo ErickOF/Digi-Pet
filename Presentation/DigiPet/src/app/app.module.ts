e551c20f42669615a2219d312812777a94585ec1
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-06-05 23:25:03 -0600

    Login: Created

--- Presentation/DigiPet/src/app/app.module.ts
+++ Presentation/DigiPet/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { Urls } from './configuration/urls';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
