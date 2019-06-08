6009c1b559e9a88eeedf1c101867f71ed2b8b9a2
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-06-08 14:07:34 -0600

    AppModule: Update

--- Presentation/DigiPet/src/app/app.module.ts
+++ Presentation/DigiPet/src/app/app.module.ts
@@ -1,42 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TabModule } from 'angular-tabs-component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthService } from './services/auth/auth.service';
+import { AdminGuard } from './services/auth/admin-guard/admin.guard';
+import { OwnerGuard } from './services/auth/owner-guard/owner.guard';
+import { PetCareGuard } from './services/auth/pet-care-guard/pet-care.guard';
+
+import { EncryptionService } from './services/encryption/encryption.service'
+
 import { Urls } from './configuration/urls';
-import { LoginComponent } from './login/login.component';
-import { OwnerComponent } from './owner/owner.component';
-import { LoginNavbarComponent } from './login/login-navbar/login-navbar.component';
-import { RegisterNavbarComponent } from './register/register-navbar/register-navbar.component';
+
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { LoginNavbarComponent } from './login/login-navbar/login-navbar.component';
+import { OwnerComponent } from './owner/owner.component';
 import { PetCareComponent } from './pet-care/pet-care.component';
 import { RegisterComponent } from './register/register.component';
+import { RegisterNavbarComponent } from './register/register-navbar/register-navbar.component';
 import { TabOwnerComponent } from './register/tab-owner/tab-owner.component';
 import { TabPetCareComponent } from './register/tab-pet-care/tab-pet-care.component';
 
 
 @NgModule({
   declarations: [
+    AdminComponent,
     AppComponent,
     LoginComponent,
-    OwnerComponent,
     LoginNavbarComponent,
-    RegisterNavbarComponent,
-    AdminComponent,
+    OwnerComponent,
     PetCareComponent,
     RegisterComponent,
+    RegisterNavbarComponent,
     TabOwnerComponent,
     TabPetCareComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TabModule
   ],
   providers: [
+    AdminGuard,
+    AuthService,
+    EncryptionService,
+    OwnerGuard,
+    PetCareGuard,
   	Urls
   ],
   bootstrap: [
