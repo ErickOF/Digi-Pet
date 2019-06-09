7731711c9a2db30cc7403c61c2bbf8e771c8baeb
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-06-08 20:33:51 -0600

    Deploy configurations

--- Presentation/DigiPet/server.js
+++ Presentation/DigiPet/server.js
@@ -1,11 +1,12 @@
-const expreses = require('express');
+const express = require('express');
+const app = express();
+
 const path = require('path');
 
-const app = express();
-app.use(express.static(__dirname + 'dist'));
-app.listen(process.env.PORT || 10000);
-app.get('/*', function(request, response) {
-	response.sendFile(path.join(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + '/dist/DigiPet'));
+app.listen(process.env.PORT || 8080);
+app.get('*', function(req, res) {
+	resp.sendFile(path.join(__dirname + '/dist/DigiPet/index.html'));
 });
 
 console.log('Listenting!');
