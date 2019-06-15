42a5f0ac510edd14f36f2d567fdc502fe7e5323b
Allan <anavarro3106@gmail.com>
2019-06-15 13:01:45 -0600

    Dto que se devuelve al crear caminata

---
+++ BackEnd/BackEnd/Dtos/WalkInfoDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Dtos
+{
+    public class WalkInfoDto
+    {
+
+        public int Id { get; set; }
+        public int PetId { get; set; }
+
+        public int WalkerId { get; set; }
+
+        public DateTime Begin { get; set; }
+
+        public decimal Duration { get; set; }
+        
+        public string Province { get; set; }
+
+        public string Canton { get; set; }
+  
+        public string Description { get; set; }
+    }
+}
