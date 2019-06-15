be09c8c9b643277916728b4f073e7667406cb159
Allan <anavarro3106@gmail.com>
2019-06-15 13:01:05 -0600

    modelo para configuracion en DB

---
+++ BackEnd/BackEnd/Model/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class AppConfiguration
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Property { get; set; }
+        [Required]
+        public string Value { get; set; }
+
+    }
+}
