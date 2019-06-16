7487de41524b6bfbf27bd8b3c070ba7a4c1a3093
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-06-15 20:14:11 -0600

    Add Pet

--- Presentation/DigiPet/src/app/owner/owner-pets/owner-pets.component.ts
+++ Presentation/DigiPet/src/app/owner/owner-pets/owner-pets.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 import { DataTransferService } from './../../services/data-transfer/data-transfer.service';
 
 
+window.onclick = function(event) {
+	let modal = document.getElementById("myModal");
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
 @Component({
 	selector: 'app-owner-pets',
 	templateUrl: './owner-pets.component.html',
 	styleUrls: ['./owner-pets.component.css']
 })
 export class OwnerPetsComponent implements OnInit {
-
 	public pets;
 
 	constructor(private dataTransferService: DataTransferService) {
@@ -17,7 +23,6 @@ export class OwnerPetsComponent implements OnInit {
 		for (let i = 0; i < this.pets.length; i++) {
 			this.pets[i].dateCreated = this.pets[i].dateCreated.split('T')[0];
 		}
-		console.log(this.pets[0]);
 	}
 
 	ngOnInit() {
