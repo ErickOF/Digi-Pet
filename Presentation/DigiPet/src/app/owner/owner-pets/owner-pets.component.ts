58d4d5833110a468cbb7bc0f9e9cd82ebeba35e1
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-06-15 20:39:01 -0600

    Add Pet: Update

--- Presentation/DigiPet/src/app/owner/owner-pets/owner-pets.component.ts
+++ Presentation/DigiPet/src/app/owner/owner-pets/owner-pets.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgxLoadingComponent } from 'ngx-loading';
+import Swal from 'sweetalert2';
 
+import { ApiService } from './../../services/api/api.service';
+import { AuthService } from './../../services/auth/auth.service';
 import { DataTransferService } from './../../services/data-transfer/data-transfer.service';
+import { ImgUploadService } from './../../services/uploads/img-upload/img-upload.service';
 
 
 window.onclick = function(event) {
@@ -17,14 +23,45 @@ window.onclick = function(event) {
 })
 export class OwnerPetsComponent implements OnInit {
 	public pets;
+	public registerPet: FormGroup;
 
-	constructor(private dataTransferService: DataTransferService) {
+	constructor(private formBuilder: FormBuilder,
+				private api: ApiService,
+				private authService: AuthService,
+				private dataTransferService: DataTransferService,
+				private imgUploadService: ImgUploadService) {
 		this.pets = this.dataTransferService.getUserInformation().pets;
 		for (let i = 0; i < this.pets.length; i++) {
 			this.pets[i].dateCreated = this.pets[i].dateCreated.split('T')[0];
 		}
+
+		this.registerPet = this.formBuilder.group({
+			name: ['', Validators.compose([
+				Validators.required,
+				Validators.maxLength(30)
+			])],
+			breed: ['', Validators.compose([
+				Validators.required,
+				Validators.maxLength(30)
+			])],
+			age: ['', Validators.compose([
+				Validators.required,
+				Validators.pattern('^[0-9]*$')
+			])],
+			size: 'S',
+			photos: '',
+			description: ['', Validators.maxLength(300)]
+		});
 	}
 
 	ngOnInit() {
+	}
+
+	public hideModal() {
+		document.getElementById('myModal').style.display='none';
+	}
+
+	public showModal() {
+		document.getElementById('myModal').style.display='block';
 	}
 }
