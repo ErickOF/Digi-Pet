3bfcdcce837228d0386947059c00000bd0186400
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-06-16 18:20:45 -0600

    Pending Reports Card: View

--- Presentation/DigiPet/src/app/pet-care/pet-care-reports/pet-care-reports.component.ts
+++ Presentation/DigiPet/src/app/pet-care/pet-care-reports/pet-care-reports.component.ts
@@ -1,8 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgxLoadingComponent } from 'ngx-loading';
+import { Observable } from "rxjs";
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
+
+import { ApiService } from './../../services/api/api.service';
 import { DataTransferService } from './../../services/data-transfer/data-transfer.service';
+import { ImgUploadService } from './../../services/uploads/img-upload/img-upload.service';
 import { UsersService } from './../../services/api/users/users.service';
 
+
+window.onclick = function(event) {
+	let reportCardModal = document.getElementById("ReportCardModal");
+	
+	if (event.target == reportCardModal) {
+		reportCardModal.style.display = "none";
+	}
+}
 
 @Component({
 	selector: 'app-pet-care-reports',
@@ -10,19 +26,32 @@ import { UsersService } from './../../services/api/users/users.service';
 	styleUrls: ['./pet-care-reports.component.css']
 })
 export class PetCareReportsComponent implements OnInit {
-
+	public idPendingReportCard = 0;
+	public registerPendingReportCard: FormGroup;
 	public reports: any = [];
 
-	constructor(private dataTransferService: DataTransferService,
+	constructor(private api: ApiService,
+				private dataTransferService: DataTransferService,
+				private formBuilder: FormBuilder,
+				private imgUploadService: ImgUploadService,
+				private router: Router,
 				private usersService: UsersService) {
+
 		let token = this.dataTransferService.getAccessToken().token;
 		let response = this.usersService.getPendingReportCards(token);
 
 		response.subscribe(data => {
 			this.reports = data;
-			console.log(data);
 		}, error => {
 			console.log(error);
+		});
+
+		this.registerPendingReportCard = this.formBuilder.group({
+			distance: ['', Validators.compose([
+				Validators.required,
+				Validators.pattern('^[0-9]*$')
+			])],
+			comments: ['', Validators.maxLength(300)]
 		});
 		
 	}
@@ -30,4 +59,20 @@ export class PetCareReportsComponent implements OnInit {
 	ngOnInit() {
 	}
 
+	get pendingReportCardFormControls() {
+		return this.registerPendingReportCard.controls;
+	}
+
+	public fillPendingReportCard(reportCard) {
+		this.idPendingReportCard = reportCard.id;
+		this.showReportCardModal();
+	}
+
+	public hideReportCardModal() {
+		document.getElementById('ReportCardModal').style.display='none';
+	}
+
+	public showReportCardModal() {
+		document.getElementById('ReportCardModal').style.display='block';
+	}
 }
